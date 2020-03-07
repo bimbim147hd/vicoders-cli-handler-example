5e06cb7266ac76cf885e2336b1fe8867836d995a
Bao Trung <baotrung@Baos-MacBook-Pro.local>
2020-03-07 11:09:30 +0700

    add cmd

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Rule, chain, apply, url, move, mergeWith, applyTemplates, filter, noop } from '@angular-devkit/schematics';
 import { strings } from '@angular-devkit/core';
 import { common } from '@vicoders/cli-support';
-import * as path from 'path';
 
 export default function handler(options: any): Rule {
   const templateSource = apply(url('./files/slider'), [
@@ -12,12 +11,6 @@ export default function handler(options: any): Rule {
     }),
     move(options.path)
   ]);
-  return chain([
-    mergeWith(templateSource),
-    common.console.exec('ls -al'),
-    common.ulti.download('https://github.com/vicodersvn/vicoders-cli-handler-example/archive/master.zip', path.resolve(process.cwd(), 'master.zip')),
-    common.console.exec(`mkdir -p ${path.resolve(process.cwd(), 'test')}`),
-    common.ulti.sleep(5000),
-    common.ulti.decompress(path.resolve(process.cwd(), 'master.zip'), path.resolve(process.cwd(), 'test'))
-  ]);
+  console.log({ options });
+  return chain([mergeWith(templateSource), common.console.exec(`vc2-wp plugin install ${options.plugin}`)]);
 }
